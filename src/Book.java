5e7fbfc84da3b382d3742adbaa8a482ffde5d84b
acer Notebook <acer Notebook@FEEN>
2018-03-05 03:56:43 +0700

    ส่งข้อ 1และ 2 ครับ

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+
+public class Book {
+	String title = "No name";
+	String type = "No type";
+	
+	public Book() {
+		
+	}
+	public Book(String title,String type) {
+		this.title = title;
+		this.type = type;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getType() {
+		return type;
+	}
+}
---
+++ src/Register.java
@@ -0,0 +1,21 @@
+
+public class Register  {
+	String grade = "None";
+	static Student fromStudent;
+	static Course toCourse;
+	
+	public Register(Student fromStudent,Course toCourse) {
+		this.fromStudent = fromStudent;
+		this.toCourse = toCourse;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public void setGrade(int score) {
+		this.grade = grade;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	
+}
