c5582cce90a61f4421cf168ae68d08f0bf0ca5f5
sudarat.sp <sudarat.sp@DESKTOP-6NK8GSL>
2018-03-09 00:35:22 +0700

    ข้อ3

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+
+public class Student  {
+		
+	private String id;
+    private String name;
+	
+	public  Student() {
+	
+	}
+	public Student(String id,String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+	public void setId(String id) {
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+}
